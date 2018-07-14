f28afad27c83f026ba4c313a4384ca43c4b57c35
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-07-14 13:18:44 +0200

    Create Prenos_Parametara_Po_Adresi.c

---
+++ Prenos_Parametara_Po_Adresi.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void prepisi(int *, int *); // Prototip funkcije
+
+
+int main(void)  {
+    int a = 100;
+    int b = 200;
+
+    printf("Pre poziva funkcije prepisi(), vrednost a je: %d\n", a);
+    printf("Pre poziva funkcije prepisi(), vrednost b je: %d\n\n", b);
+
+    prepisi(&a, &b); // Pozivamo funkciju prepisi()
+                   // Parametre prosleđujemo po adresi
+
+    printf("Posle poziva funkcije prepisi(), vrednost a je: %d\n", a);
+    printf("Posle poziva funkcije prepisi(), vrednost b je: %d\n", b);
+
+    return 0;
+}
+
+void prepisi(int *x, int *y) { // Definicija naše funkcije prepisi()
+    *x = 500; // Radimo sa originalima
+    *y = 600; // Originali će sada biti promenjeni!
+
+    return;
+}
