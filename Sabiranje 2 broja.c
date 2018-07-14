e91cf5a55df12412a60c761470fa404a6a16d0c2
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-07-14 13:24:56 +0200

    Create Sabiranje 2 broja.c

---
+++ Sabiranje 2 broja.c
@@ -0,0 +1,11 @@
+int  main(void) 
+{  
+    int broj1, broj2, rezultat;  
+    broj1 = 8;
+    broj2 = 16;  
+    rezultat = broj1 + broj2;  
+    
+    printf("Zbir %d + %d = %d \n", broj1, broj2, rezultat); 
+
+    return 0; 
+}
