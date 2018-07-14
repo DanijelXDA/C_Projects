737f516547c921b9b05cbfdabeca3e523308a0e1
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-07-14 13:28:54 +0200

    Create Funkcije za rad sa karakterima.c

---
+++ Funkcije za rad sa karakterima.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    char karakter;
+
+    karakter = getchar(); // Učitava jedan karakter sa standardnog ulaza
+    putchar(karakter);    // Ispisuje jedan karakter na ekran
+
+    return 0;
+}
