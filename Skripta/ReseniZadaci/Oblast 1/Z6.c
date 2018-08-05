20de62e169dc039cf103a9229d35ab38d3d6bd82
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-08-05 22:15:28 +0200

    Add files via upload

---
+++ Skripta/ReseniZadaci/Oblast 1/Z6.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // Pretprocesorska direktiva
+#include <math.h> // Matematicka C biblioteka
+
+int main(void) // Glavna funkcija programa
+{
+    printf("Vrednost broja Pi iznosi: %.5lf.\n", M_PI); // Ispis konstante Pi iz biblioteke math.h
+    return 0;
+}
