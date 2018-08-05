1a32a35ee6364be2c7d2e74a0d6255b55ff26fd7
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-08-05 22:13:08 +0200

    Create Z1.c

---
+++ Skripta/ReseniZadaci/Oblast 1/Z1.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // Pretprocesorska direktiva
+
+int main(void) // Glavna funkcija programa
+{
+    int i; // Promenljiva
+
+    for(int i = 1; i <= 5; i++) // Koristimo za ponavljanje naredbe 5 puta
+        printf("Ja volim programiranje!\n"); // Ispis na konzolu
+
+    return 0;
+}
