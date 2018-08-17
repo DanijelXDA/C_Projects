c405c4e84abeb31c7b0aac0f297dbbbd563d45c2
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-08-17 13:19:40 +0200

    uvod u pokazivace

---
+++ Skripta_Reseni_Zadaci/uvod u pokazivace.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int  var1;
+    char var2[10];
+
+    printf("Adresa promenljive var1 je: %x\n", &var1);
+    printf("Adresa promenljive var2 je: %x\n", &var2);
+
+    return 0;
+}
+
