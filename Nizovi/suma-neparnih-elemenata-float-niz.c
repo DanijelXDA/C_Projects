eaa4d554abe39e15f250bf4ed738f28d3bec4e75
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-09-17 20:45:50 +0200

    Fix bad code

--- Nizovi/suma-neparnih-elemenata-float-niz.c
+++ Nizovi/suma-neparnih-elemenata-float-niz.c
@@ -2,29 +2,22 @@
 
 int main(void)
 {
-    // Promenljive u vezi zadatka
-    float niz[7]; // Deklaracija realnog niza
-    float zbirNeparnih = 0; // Promenljiva u koju čuvamo zbir neparnih elemenata niza
-    int i; // Promenljiva za for() petlju
-
-    printf("Unos elemenata niza\n");
-    for(i = 0; i < 7; i++)
-    {
-        printf("\tUnesite vrednost niz[%d] = ", i); // Ispis da se unese element
-        scanf("%f", &niz[i]); // Učitavanje elementa niza
+    int i;
+    float niz[7], suma = 0; // Promenljive u vezi zadatka
+    
+    
+    for(i = 0; i < 7; i++) {
+        printf("Unesite niz[%d] = ", i);
+        scanf("%f", &niz[i]);
     }
-
-    for(i = 0; i < 7; i++)
-    {
-        int konvertovanBroj = niz[i] * 100; // ako je broj 5.15 pomnožimo sa 100 da bi dobili 515
-        // i tada znamo da taj broj nije deljiv sa 2 tj. neparan je!
-
-        if( ( konvertovanBroj % 2) != 0 ) // Provera da li je element niza neparan
-            zbirNeparnih += niz[i]; // Dodaj neparan element u sumu
-        // Radi samo za realne brojeve do 2 decimale!!!
+    
+    i = 0; // Vraćanje na vrednost 0!
+    while(i < 7) {
+        suma += niz[i];
+        i += 2;
     }
-
-    printf("\nZbir neparnih elemenata je: %.2f", zbirNeparnih);
-
+        
+    printf("Suma iznosi: %f", suma);
+        
     return 0;
 }
