3789bedcb6543a28b65c3d0a78193336b0e829e8
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-09-17 20:37:10 +0200

    Init v2

--- Nizovi/prikaz-treceg-elementa-u-nizu.c
+++ Nizovi/prikaz-treceg-elementa-u-nizu.c
@@ -2,18 +2,15 @@
 
 int main(void)
 {
-    // Promenljive u vezi zadatka
-    int niz[8]; // Deklaracija celobrojnog niza
-    int i; // Promenljiva za for() petlju
-
-    printf("Unos elemenata niza\n");
-    for(i = 0; i < 8; ++i)
-    {
-        printf("\tUnesite vrednost niz[%d] = ", i); // Ispis da se unese element
-        scanf("%d", &niz[i]); // Učitavanje elementa niza
+    int i, niz[8]; // Promenljive u vezi zadatka
+    
+    
+    for(i = 0; i < 8; i++) {
+        printf("Unesite niz[%d] = ", i);
+        scanf("%d", &niz[i]);
     }
-
-    printf("\nTreci element je: %d", niz[2]);
-
+    
+    printf("Treci element je: %d ", niz[2]);
+        
     return 0;
 }
